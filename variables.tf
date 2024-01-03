29e21dc17161913d55d0907f306b56bdb3aca47e
Aldans <seopakc.05@gamil.com>
2024-01-03 17:21:26 +0000

    task 7.2

--- variables.tf
+++ variables.tf
@@ -9,26 +9,8 @@ variable "GOOGLE_REGION" {
   description = "GCP region to use"
 }
 
-variable "GKE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type"
-}
-
 variable "GKE_NUM_NODES" {
   type        = number
   default     = 2
   description = "GKE nodes number"
-}
-
-variable "GKE_CLUSTER_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE cluster name"
-}
-
-variable "GKE_POOL_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE pool name"
 }
